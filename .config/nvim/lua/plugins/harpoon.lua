c28b187025d9f6509208412d8b5963569bce72be
Mantas Gricius <mantasgricius6@gmail.com>
2026-02-28 12:15:45 +0200

    config LazyVim way

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -3,21 +3,47 @@ return {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
         dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {},
-        config = function(_, opts)
+        opts = {
+            settings = {
+                save_on_toggle = true,
+                sync_on_ui_close = true,
+            },
+        },
+        keys = function()
             local harpoon = require("harpoon")
-
-            -- REQUIRED
-            harpoon:setup()
-            -- REQUIRED
-
-            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-            vim.keymap.set("n", "<leader>s", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-            vim.keymap.set("n", "<leader>q", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<leader>w", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<leader>e", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<leader>r", function() harpoon:list():select(4) end)
-        end
+            local keys = {
+                {
+                    "<leader>a",
+                    function() harpoon:list():add() end,
+                    desc = "Add a file to harpoon list",
+                },
+                {
+                    "<leader>h",
+                    function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+                    desc = "Toggle harpoon menu",
+                },
+                {
+                    "<leader>q",
+                    function() harpoon:list():select(1) end,
+                    desc = "Select 1st file in harpoon list"
+                },
+                {
+                    "<leader>w",
+                    function() harpoon:list():select(2) end,
+                    desc = "Select 2nd file in harpoon list"
+                },
+                {
+                    "<leader>e",
+                    function() harpoon:list():select(3) end,
+                    desc = "Select 3rd file in harpoon list"
+                },
+                {
+                    "<leader>r",
+                    function() harpoon:list():select(4) end,
+                    desc = "Select 4th file in harpoon list"
+                },
+            }
+            return keys
+        end,
     }
 }
