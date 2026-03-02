cac5390578d6da43bec6c32bbdc9e156e441c82e
Mantas Gricius <mantasgricius6@gmail.com>
2026-03-02 21:50:49 +0200

    upgrade lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,19 +1,50 @@
 return {
   {
-    'nvim-lualine/lualine.nvim',
-    opts = function()
-      local opts = {
-        options = {
-          theme = 'horizon',
-          component_separators = { left = '|', right = '|'},
-          section_separators = { left = '', right = ''},
-        }
+    "letieu/harpoon-lualine",
+    dependencies = {
+      {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
       }
-      return opts
-    end
+    },
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        theme = 'material',
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+      },
+      sections = {
+        lualine_c = {
+          '%=',
+          {
+            "harpoon2",
+            icon = ' ',
+            indicators = { "Q", "W", "E", "R" },
+            active_indicators = { "[Q]", "[W]", "[E]", "[R]" },
+            color_active = { fg = "#ba181b" },
+            _separator = " ",
+            no_harpoon = "Harpoon not loaded",
+          },
+        },
+        lualine_x = {
+          {
+            'fileformat',
+            icons_enabled = true,
+            symbols = {
+              unix = 'LF',
+              dos = 'CRLF',
+              mac = 'CR',
+            },
+          },
+        },
+      }
+    },
   },
   {
     'nvim-tree/nvim-web-devicons',
     opts = {}
-  }
+  },
 }
