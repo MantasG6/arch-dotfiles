840858901fe8fa6fc20e6542c651fe334f914b7c
Mantas Gricius <mantasgricius6@gmail.com>
2026-04-30 08:50:19 +0300

    fix bt-fast-conn

--- .other/bt-fast-conn/toggle_bt_fast_connect.sh
+++ .other/bt-fast-conn/toggle_bt_fast_connect.sh
@@ -2,6 +2,14 @@
 
 AC_STATE=$(cat /sys/class/power_supply/AC/online)
 
+# Wait for hci0 to be ready (up to 10s)
+i=0
+while ! bluetoothctl show | grep -q "Controller"; do
+  sleep 0.5
+  i=$((i + 1))
+  [ "$i" -ge 20 ] && echo "Timeout waiting for BT controller" && exit 1
+done
+
 if [ "$AC_STATE" -eq 1 ]; then
     /usr/bin/bluetoothctl mgmt.fast-conn on
 else
