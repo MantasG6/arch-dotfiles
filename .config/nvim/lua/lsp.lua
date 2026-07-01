45ec6029c19f36aab736b62d5c6a0fe412e68751
Mantas Gricius <mantasgricius6@gmail.com>
2026-07-01 21:42:15 +0300

    fix gd

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -3,3 +3,8 @@ vim.diagnostic.config({
         prefix = '',
     },
 })
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf, desc = 'Go to definition' })
+  end,
+})
